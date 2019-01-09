0f6290d3def5a0f6f2a0eef38e38860c616a693b
Matthew David Miller <matthewdavidmiller1@gmail.com>
2019-01-09 06:16:33 -0500

    Update Raspberry_Pi_automatic_updates.sh

--- Raspberry_Pi_automatic_updates.sh
+++ Raspberry_Pi_automatic_updates.sh
@@ -27,7 +27,7 @@
 # Run the script as root
 
 # Create a temporary Directory
-mkdir /temp/scripttemp
+mkdir -p /tmp/scripttemp
 
 # Variable for words to search for when updating.
 search_for_these_words="0 upgraded"
@@ -37,14 +37,11 @@ search_for_these_words="0 upgraded"
 file_to_search_for_words=/temp/scripttemp/temp.txt
 
 
-/usr/bin/apt-get update && /usr/bin/rpi-update && /usr/bin/apt-get upgrade -y > ${file_to_search_for_words} | (while true ; do
-tail -f -c 0 ${file_to_search_for_words} | read -r apps_updated
-printf ${apps_updated} | grep -q ${search_for_these_words}
+/usr/bin/apt-get update && /usr/bin/rpi-update && /usr/bin/apt-get upgrade -y |& tee -a ${file_to_search_for_words}
+grep -qi ${search_for_these_words} ${file_to_search_for_words}
 if [ "$?" = "1" ] ; then
 /usr/bin/apt-get autoremove --purge
 /usr/bin/apt-get autoclean
-rm -rf /temp/scripttemp
+rm -rf /tmp/scripttemp
 /sbin/reboot
 fi
-done
-)
