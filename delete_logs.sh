3a9896dc9df029807563982aba41ea3e400b936e
Matthew David Miller <matthewdavidmiller1@gmail.com>
2019-01-19 14:25:27 -0500

    Update delete_logs.sh

--- delete_logs.sh
+++ delete_logs.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 # Simple script to clear log files I have setup.
-/usr/bin/find /home/matthew/logs/cronlog_reloadufw.log -mtime +30 -delete
-/usr/bin/find /home/matthew/logs/cronlog_update.log -mtime +30 -delete
-/usr/bin/find /home/matthew/logs/cronlog_send_ip_address.log -mtime +30 -delete
-/usr/bin/find /home/matthew/logs/freedns_matthewmiller_us_to.log -mtime +30 -delete
-/usr/bin/find /home/matthew/logs/get_email_from_vpn_connections.log -mtime +30 -delete
+
+# Variables
+reload_ufw_log="/home/matthew/logs/cronlog_reloadufw.log"
+update_log="/home/matthew/logs/cronlog_update.log"
+send_ip_address_log="/home/matthew/logs/cronlog_send_ip_address.log"
+freedns_log="/home/matthew/logs/freedns_matthewmiller_us_to.log"
+email_vpn_connections_log="/home/matthew/logs/get_email_from_vpn_connections.log"
+
+# Script to delete logs
+/usr/bin/find "${reload_ufw_log}" -mtime +30 -delete
+/usr/bin/find "${update_log}" -mtime +30 -delete
+/usr/bin/find "${send_ip_address_log}" -mtime +30 -delete
+/usr/bin/find "${freedns_log}" -mtime +30 -delete
+/usr/bin/find "${email_vpn_connections_log}" -mtime +30 -delete
