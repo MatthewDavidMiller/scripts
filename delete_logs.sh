2321fb178b0381dcd6c553da5c51b6b3eff22447
Matthew David Miller <matthewdavidmiller1@gmail.com>
2019-01-21 10:33:01 -0500

    Updated

--- delete_logs.sh
+++ delete_logs.sh
@@ -5,9 +5,9 @@
 # Simple script to clear log files I have setup.
 
 # Variables
-reload_ufw_log="/home/matthew/logs/cronlog_reloadufw.log"
-update_log="/home/matthew/logs/cronlog_update.log"
-send_ip_address_log="/home/matthew/logs/cronlog_send_ip_address.log"
+reload_ufw_log="/home/matthew/logs/reloadufw.log"
+update_log="/home/matthew/logs/update.log"
+send_ip_address_log="/home/matthew/logs/send_ip_address.log"
 freedns_log="/home/matthew/logs/freedns_matthewmiller_us_to.log"
 email_vpn_connections_log="/home/matthew/logs/get_email_from_vpn_connections.log"
 
