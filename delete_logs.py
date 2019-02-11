50e033f117063c5fa7261c77c7c31a51a57623bf
Matthew David Miller <matthewdavidmiller1@gmail.com>
2019-02-10 22:46:44 -0500

    Now prints date. Useful when using logs

--- delete_logs.py
+++ delete_logs.py
@@ -12,6 +12,12 @@ log_3 = '/home/matthew/logs/freedns_matthewmiller_us_to.log'
 log_4 = '/home/matthew/logs/get_email_from_vpn_connections.log'
 find_command = '/usr/bin/find'
 
+# Prints date
+
+date = subprocess.check_output(['date', '+"%m/%d/%Y %H:%M:%S"'])
+
+print(date)
+
 # Script to delete logs
 
 subprocess.check_output([find_command, log_1, '-size', '40M', '-delete'])
