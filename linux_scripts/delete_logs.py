007433b64f201ee4fce9f6089922398b4a2dbbb9
Matthew David Miller <matthewdavidmiller1@gmail.com>
2019-03-08 00:07:08 -0500

    Changes in formatting.

--- linux_scripts/delete_logs.py
+++ linux_scripts/delete_logs.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2019 Matthew David Miller. All rights reserved.
+
+# Licensed under the MIT License.
+
 # Simple script to clear log files I have setup.
 
 # Libraries to import.
@@ -24,25 +28,3 @@ subprocess.check_output([find_command, log_1, '-size', '40M', '-delete'])
 subprocess.check_output([find_command, log_2, '-size', '40M', '-delete'])
 subprocess.check_output([find_command, log_3, '-size', '40M', '-delete'])
 subprocess.check_output([find_command, log_4, '-size', '40M', '-delete'])
-
-# Copyright (c) 2019 Matthew David Miller. All rights reserved.
-
-# MIT License
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
--- linux_scripts/email_on_vpn_connections.sh
+++ linux_scripts/email_on_vpn_connections.sh
@@ -1,3 +1,7 @@
+# Copyright (c) 2019 Matthew David Miller. All rights reserved.
+
+# Licensed under the MIT License.
+
 #!/bin/bash
 
 # This script is used to email the admin when a VPN connection is established on the VPN server.
@@ -37,25 +41,3 @@ if [ "$?" = "0" ] ; then
 fi
 done
 )
-
-# Copyright (c) 2019 Matthew David Miller. All rights reserved.
-
-# MIT License
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
