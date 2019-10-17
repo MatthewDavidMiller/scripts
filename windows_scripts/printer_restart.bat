98bcbc38a17b47e775824edca0919ba2b1492dbb
Matthew David Miller <matthewdavidmiller1@gmail.com>
2019-10-17 05:53:55 -0400

    Now kills printer process so it can delete the tmp files.

--- windows_scripts/printer_restart.bat
+++ windows_scripts/printer_restart.bat
@@ -1,5 +1,6 @@
 rem Used to restart print spooler service.
 net stop "Print Spooler"
+taskkill /IM "printfilterpipelinesvc.exe" /F
 del /S /Q "C:\Windows\System32\spool\PRINTERS\*"
 net start "Print Spooler"
 pause
