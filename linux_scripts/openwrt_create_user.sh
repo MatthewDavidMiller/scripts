e55243bb3051ef3357700f2358e997e65b8e954b
Matthew David Miller <matthew@Matt-PC.localdomain>
2019-12-07 16:32:15 -0500

    Now gives proper ownership of the home folder to the user.

--- linux_scripts/openwrt_create_user.sh
+++ linux_scripts/openwrt_create_user.sh
@@ -9,6 +9,7 @@ printf '%s\n' "$user_name:x:700:100:$user_name:/home/$user_name:/bin/ash" >> '/e
 mkdir /home
 mkdir /home/$user_name
 passwd $user_name
+chown "$user_name" "/home/$user_name"
 
 # Setup sudo
-printf '%s\n' "${user_name} ALL=(ALL) NOPASSWD:ALL" >> '/etc/sudoers'
+printf '%s\n' "$user_name ALL=(ALL) NOPASSWD:ALL" >> '/etc/sudoers'
