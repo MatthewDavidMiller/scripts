630e7884725923f9fc3d5ad559bccbac09d4e9d9
Matthew David Miller <matthew@Matt-PC.localdomain>
2019-12-07 01:02:10 -0500

    New script to configure gdm.

---
+++ linux_scripts/configure_gdm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to configure gdm
+
+# Get username
+user_name=$(who am i | awk '{print $1}')
+
+# Enable gdm
+systemctl enable gdm.service
+
+# Enable tap to click
+gdm gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+
+# Enable autologin
+cat <<EOF > '/etc/gdm/custom.conf'
+# Enable automatic login for user
+[daemon]
+AutomaticLogin=$user_name
+AutomaticLoginEnable=True
+
+EOF
+
+# Setup default session
+cat <<EOF > "/var/lib/AccountsService/users/$user_name"
+XSession=i3
+
+EOF
