b7f0c282d8c59b05d49ced1960e600646045bc1c
Matthew David Miller <matthew@Matt-PC.localdomain>
2019-12-08 20:20:54 -0500

    New windows script. Also updated the tar batch process script.

--- windows_scripts/tar_batch_process_files.bat
+++ windows_scripts/tar_batch_process_files.bat
@@ -1,3 +1,8 @@
 rem Used to process multiple files and folders wherever the bat file is located and run from.
 for /d %%X in (*) do tar -cf "%%X.tar" "%%X"
+
+CHOICE /M "Compress tar archives? "
+IF %errorlevel% equ 1 (
+    for %%i in (*.tar) do "c:\Program Files\7-Zip\7z.exe" a "%%i.7z" "%%i" -mx=9
+)
 pause
