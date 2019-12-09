f17a552311bda08ed090c0bcf0a2736b3c159609
Matthew David Miller <matthew@Matt-PC.localdomain>
2019-12-08 21:00:19 -0500

    Script now compresses before placing folders into tar archive.

--- windows_scripts/tar_batch_process_files.bat
+++ windows_scripts/tar_batch_process_files.bat
@@ -1,8 +1,13 @@
 rem Used to process multiple files and folders wherever the bat file is located and run from.
-for /d %%X in (*) do tar -cf "%%X.tar" "%%X"
 
-CHOICE /M "Compress tar archives? "
+CHOICE /M "Compress folders with 7zip? "
 IF %errorlevel% equ 1 (
-    for %%i in (*.tar) do "c:\Program Files\7-Zip\7z.exe" a "%%i.7z" "%%i" -mx=9
+    for /d %%X in (*) do "c:\Program Files\7-Zip\7z.exe" a "%%X.7z" "%%X" -mx=9
+    for %%i in (*.7z) do tar -cf "%%i.tar" "%%i"
+    del *.7z
+)
+
+IF %errorlevel% equ 2 (
+    for /d %%X in (*) do tar -cf "%%X.tar" "%%X"
 )
 pause
