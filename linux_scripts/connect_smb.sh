182aff4dd6bd3199311de6da1b07408e4cd34679
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-11 10:40:31 -0500

    New script to connect and automount a samba share.

---
+++ linux_scripts/connect_smb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to connect and mount a smb share
+
+# Variables to edit
+# Share location
+share='//matt-nas.lan/matt_files'
+# Mount point location
+mount_location='/mnt/matt-nas'
+# Username
+username='matthew'
+# Password
+password='password'
+
+# Install samba
+pacman -S samba
+
+# Make directory to mount the share at
+mkdir "${mount_location}"
+
+# Automount smb share
+printf '%s\n' "${share} /mnt/matt-nas cifs rw,user,username=${username},password=${password} 0 0" >> '/etc/fstab'
