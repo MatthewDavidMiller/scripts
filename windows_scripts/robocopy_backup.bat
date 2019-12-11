97d5141f05166e43d87a098838f42da2cee4d786
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-11 06:05:41 -0500

    New script to copy a directory to another location.

---
+++ windows_scripts/robocopy_backup.bat
@@ -0,0 +1,9 @@
+rem Used to backup files
+rem Run the script as administrator
+
+rem Set source and destination
+SET source_directory="source_location"
+SET destination_directory="destination_location"
+SET log_file="log_file"
+
+robocopy %source_directory% %destination_directory% /s /copyall /np /r:2 /w:5 /tee /log:%log_file%
