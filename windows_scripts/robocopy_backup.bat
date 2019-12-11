c8ff9eaa0a6d4794f71710802bf8afb0ffda3681
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-11 06:09:04 -0500

    Updated a comment.

--- windows_scripts/robocopy_backup.bat
+++ windows_scripts/robocopy_backup.bat
@@ -1,7 +1,7 @@
 rem Used to backup files
 rem Run the script as administrator
 
-rem Set source and destination
+rem Set source, destination, and log file location
 SET source_directory="source_location"
 SET destination_directory="destination_location"
 SET log_file="log_file"
