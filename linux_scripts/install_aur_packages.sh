04682e62a8ce07f035539471a76bfcba0b5cb15d
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-12 04:27:44 -0500

    Two new scripts to install and update aur packages for Arch Linux.

---
+++ linux_scripts/install_aur_packages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Script to to install packages from Arch Linux aur.
+# Verify the source for malicious content before using.
+
+# Get username
+user_name=$(logname)
+
+# Install packages
+pacman -S --needed base-devel
+
+# Create a directory to use for compiling aur packages
+mkdir "/home/${user_name}/aur"
+chown "${user_name}" "/home/${user_name}/aur"
+chmod 744 "/home/${user_name}/aur"
+
+# Install freefilesync
+read -r -p "Install freefilesync? [y/N] " response
+if [[ "${response}" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        mkdir "/home/${user_name}/aur/freefilesync"
+        git clone 'https://aur.archlinux.org/freefilesync.git' "/home/${user_name}/aur/freefilesync"
+        chown -R "${user_name}" "/home/${user_name}/aur/freefilesync"
+        chmod -R 744 "/home/${user_name}/aur/freefilesync"
+        cd "/home/${user_name}/aur/freefilesync" || exit
+        read -r -p "Check the contents of the files before installing. Press enter to continue: "
+        less PKGBUILD
+        read -r -p "Ready to install? [y/N] " response2
+        if [[ "${response2}" =~ ^([yY][eE][sS]|[yY])+$ ]]
+            then
+                sudo -u ${user_name} makepkg -si
+        fi
+fi
---
+++ linux_scripts/update_aur_packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script to to update packages from Arch Linux aur.
+# Verify the source for malicious content before using.
+
+# Get username
+user_name=$(logname)
+
+# Install packages
+pacman -S --needed base-devel
+
+# Update freefilesync
+read -r -p "Update freefilesync? [y/N] " response
+if [[ "${response}" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        cd "/home/${user_name}/aur/freefilesync" || exit
+        git pull
+        read -r -p "Check the contents of the files before installing. Press enter to continue: "
+        less PKGBUILD
+        read -r -p "Ready to update? [y/N] " response2
+        if [[ "${response2}" =~ ^([yY][eE][sS]|[yY])+$ ]]
+            then
+                sudo -u ${user_name} makepkg -si
+        fi
+fi
