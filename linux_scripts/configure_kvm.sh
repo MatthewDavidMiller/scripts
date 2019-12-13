4409a9d083fe2c4ceef2efa7d7e0c74dac7de9cc
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-12 21:46:37 -0500

    Script now enables nested support.

--- linux_scripts/configure_kvm.sh
+++ linux_scripts/configure_kvm.sh
@@ -4,3 +4,10 @@
 
 # Install packages
 pacman -S --needed libvirt gnome-boxes ebtables dnsmasq bridge-utils
+
+# Enable nested virtualization
+cat <<EOF > '/etc/modprobe.d/kvm_intel.conf'
+
+options kvm_intel nested=1
+
+EOF
