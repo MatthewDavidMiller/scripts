474ff320cf3b6a7cbed0af5edde2d53e79ba2d52
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-13 05:25:42 -0500

    Changed formatting. Script can now add more than one samba share.

--- linux_scripts/connect_smb.sh
+++ linux_scripts/connect_smb.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-# Script to connect and mount a smb share
-
-# Prompts
-# Share location
-read -r -p "Specify share location. Example'//matt-nas.lan/matt_files': " share
-# Mount point location
-read -r -p "Specify mount location. Example'/mnt/matt-nas': " mount_location
-# Username
-read -r -p "Specify Username. Example'matthew': " username
-# Password
-read -r -p "Specify Password. Example'password': " password
-
 # Install samba
 pacman -S --noconfirm --needed samba
 
-# Make directory to mount the share at
+# Make /mnt directory
 mkdir '/mnt'
-mkdir "${mount_location}"
 
-# Automount smb share
-printf '%s\n' "${share} /mnt/matt-nas cifs rw,noauto,x-systemd.automount,_netdev,user,username=${username},password=${password} 0 0" >> '/etc/fstab'
+# Script to connect and mount a smb share
+read -r -p "Mount a samba share? [y/N] " response
+while [[ "${response}" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    do
+    # Prompts
+    # Share location
+    read -r -p "Specify share location. Example'//matt-nas.lan/matt_files': " share
+    # Mount point location
+    read -r -p "Specify mount location. Example'/mnt/matt-nas': " mount_location
+    # Username
+    read -r -p "Specify Username. Example'matthew': " username
+    # Password
+    read -r -p "Specify Password. Example'password': " password
+
+    # Make directory to mount the share at
+    mkdir "${mount_location}"
+
+    # Automount smb share
+    printf '%s\n' "${share} ${mount_location} cifs rw,noauto,x-systemd.automount,_netdev,user,username=${username},password=${password} 0 0" >> '/etc/fstab'
+    printf '%s\n' ''
+
+    # Mount another disk
+    read -r -p "Do you want to mount another samba share? [y/N] " response
+    if [[ "${response}" =~ ^([nN][oO]|[nN])+$ ]]
+        then
+            printf '%s\n' '' >> '/etc/fstab'
+            exit
+    fi
+done
