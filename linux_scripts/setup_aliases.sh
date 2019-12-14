42122879d43640c26a316acadfca5d305fbb489a
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-13 21:59:41 -0500

    Updated alias.

--- linux_scripts/setup_aliases.sh
+++ linux_scripts/setup_aliases.sh
@@ -14,7 +14,7 @@ alias sudo='sudo '
 alias ssh_nas="ssh -i '/mnt/matt-nas/SSHConfigs/matt_homelab/nas.openssh' matthew@matt-nas.lan"
 alias ssh_openwrt="ssh -i '/mnt/matt-nas/SSHConfigs/matt_homelab/openwrt.openssh' matthew@mattopenwrt.lan"
 alias ssh_proxmox="ssh -i '/mnt/matt-nas/SSHConfigs/matt_homelab/proxmox.openssh' matthew@matt-prox.lan"
-alias ssh_vpn="ssh -i '/mnt/matt-nas/SSHConfigs/matt_homelab/vpn.openssh' matthew@matt-vpn.lan"
+alias ssh_vpn="ssh -i '/mnt/matt-nas/SSHConfigs/matt_homelab/vpn.openssh' matt-vpn@matt-vpn.lan"
 alias ssh_pihole="ssh -i '/mnt/matt-nas/SSHConfigs/matt_homelab/pihole.openssh' pihole@matt-pihole.lan"
 alias pacman_autoremove='pacman -Rs $(pacman -Qtdq)'
 
