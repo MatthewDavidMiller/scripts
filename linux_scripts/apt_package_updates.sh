22cbc6f4385196e50567324516c8511b210aaef3
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2019-12-14 02:55:12 -0500

    Updated legacy updates.sh script and moved to stable.

---
+++ linux_scripts/apt_package_updates.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Used to automatically update a Debian based distro.
+# Run in cron or rc.local
+
+# Define path to commands.
+PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
+
+# Variables to edit
+temp='/tmp/apt_package_updates_temp'
+words_to_look_for='0 upgraded'
+log='/var/log/apt_package_updates.log'
+# Create temp file
+touch "${temp}"
+
+# Update the applications
+apt-get update
+apt-get upgrade -y |& tee -a "${temp}"
+if grep -qi "${words_to_look_for}" "${temp}"
+	then
+		printf '%s\n' 'No updates.' >> "${log}"
+		rm -rf "${temp}"
+	else
+		printf '%s\n' 'Updated' >> "${log}"
+		apt-get autoremove --purge
+		apt-get autoclean
+		rm -rf "${temp}"
+		reboot
+fi
