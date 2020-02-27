507d2b18f988cd6948b59a48b2108d9e2240da17
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-02-27 17:00:47 -0500

    Removed heavy reliance on chocolatey to install applications.

--- windows_scripts/configure_windows_10.ps1
+++ windows_scripts/configure_windows_10.ps1
@@ -1,7 +1,7 @@
 # Script to confgure settings in Windows 10
 
 # Prompts
-$InstallChoclatey = Read-Host 'Install Chocolatey? y/n '
+$InstallChocolatey = Read-Host 'Install Chocolatey? y/n '
 $InstallShellCheck = Read-Host 'Install ShellCheck? y/n '
 $InstallNotepadPlusPlus = Read-Host 'Install Notepad++? y/n '
 $Install7Zip = Read-Host 'Install 7Zip? y/n '
@@ -39,7 +39,6 @@ $InstallDiscord = Read-Host 'Install Discord? y/n '
 $InstallFedoraMediaWriter = Read-Host 'Install FedoraMediaWriter? y/n '
 $InstallVisualStudioCommunity = Read-Host 'Install VisualStudioCommunity? y/n '
 $InstallOpenVPN = Read-Host 'Install OpenVPN? y/n '
-$InstallVisualStudioCommunity = Read-Host 'Install VisualStudioCommunity? y/n '
 $InstallTwitch = Read-Host 'Install Twitch? y/n '
 $InstallVortex = Read-Host 'Install Vortex? y/n '
 $InstallVisualRedistributables = Read-Host 'Install Visual C++ Redistributables? y/n '
@@ -574,7 +573,7 @@ windows.immersivecontrolpanel_cw5n1h2txyewy
 }
 
 # Install chocolatey
-if ($InstallChoclatey -eq 'y') {
+if ($InstallChocolatey -eq 'y') {
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
@@ -585,72 +584,87 @@ if ($InstallShellCheck -eq 'y') {
 
 # Install notepad++
 if ($InstallNotepadPlusPlus -eq 'y') {
-    choco install notepadplusplus
+    Invoke-WebRequest 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.4/npp.7.8.4.Installer.x64.exe' -OutFile "$HOME\Downloads\notepad++_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\notepad++_installer.exe" -PassThru -Wait
 }
 
 # Install 7zip
 if ($Install7Zip -eq 'y') {
-    choco install 7zip
+    Invoke-WebRequest 'https://www.7-zip.org/a/7z1900.msi' -OutFile "$HOME\Downloads\7zip_32_bit_installer.msi"
+    Start-Process -FilePath "$HOME\Downloads\7zip_32_bit_installer.msi" -PassThru -Wait
+    Invoke-WebRequest 'https://www.7-zip.org/a/7z1900-x64.msi' -OutFile "$HOME\Downloads\7zip_64_bit_installer.msi"
+    Start-Process -FilePath "$HOME\Downloads\7zip_64_bit_installer.msi" -PassThru -Wait
 }
 
 # Install nmap
 if ($InstallNMap -eq 'y') {
-    choco install nmap
+    Invoke-WebRequest 'https://nmap.org/dist/nmap-7.80-setup.exe' -OutFile "$HOME\Downloads\nmap_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\nmap_installer.exe" -PassThru -Wait
 }
 
 # Install qbittorrent
 if ($InstallQBittorent -eq 'y') {
-    choco install qbittorrent
+    Invoke-WebRequest 'https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.2.1_x64_setup.exe' -OutFile "$HOME\Downloads\qbittorrent_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\qbittorrent_installer.exe" -PassThru -Wait
 }
 
 # Install rufus
 if ($InstallRufus -eq 'y') {
-    choco install rufus
+    Invoke-WebRequest 'https://github.com/pbatard/rufus/releases/download/v3.8/rufus-3.8.exe' -OutFile 'C:\Program Files\Rufus\rufus.exe'
 }
 
 # Install etcher
 if ($InstallEtcher -eq 'y') {
-    choco install etcher
+    Invoke-WebRequest 'https://github.com/balena-io/etcher/releases/download/v1.5.79/balenaEtcher-Setup-1.5.79.exe' -OutFile "$HOME\Downloads\etcher_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\etcher_installer.exe" -PassThru -Wait
 }
 
 # Install gimp
 if ($InstallGimp -eq 'y') {
-    choco install gimp
+    Invoke-WebRequest 'https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.18-setup.exe' -OutFile "$HOME\Downloads\gimp_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\gimp_installer.exe" -PassThru -Wait
 }
 
 # Install git
 if ($InstallGit -eq 'y') {
-    choco install git
+    Invoke-WebRequest 'https://github.com/git-for-windows/git/releases/download/v2.25.1.windows.1/Git-2.25.1-64-bit.exe' -OutFile "$HOME\Downloads\git_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\git_installer.exe" -PassThru -Wait
 }
 
 # Install vlc
 if ($InstallVlc -eq 'y') {
-    choco install vlc
+    Invoke-WebRequest 'https://videolan.mirrors.hivelocity.net/vlc/3.0.8/win64/vlc-3.0.8-win64.exe' -OutFile "$HOME\Downloads\vlc_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\vlc_installer.exe" -PassThru -Wait
 }
 
 # Install blender
 if ($InstallBlender -eq 'y') {
-    choco install blender
+    Invoke-WebRequest 'https://www.blender.org/download/Blender2.82/blender-2.82-windows64.msi/' -OutFile "$HOME\Downloads\blender_installer.msi"
+    Start-Process -FilePath "$HOME\Downloads\blender_installer.msi" -PassThru -Wait
 }
 
 # Install bitwarden
 if ($InstallBitwarden -eq 'y') {
-    choco install bitwarden
+    Invoke-WebRequest 'https://vault.bitwarden.com/download/?app=desktop&platform=windows' -OutFile "$HOME\Downloads\bitwarden_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\bitwarden_installer.exe" -PassThru -Wait
 }
 
 # Install winscp
 if ($InstallWinSCP -eq 'y') {
-    choco install winscp
+    Invoke-WebRequest 'https://winscp.net/download/WinSCP-5.17.1-Setup.exe' -OutFile "$HOME\Downloads\winscp_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\winscp_installer.exe" -PassThru -Wait
 }
 
 # Install putty
 if ($InstallPutty -eq 'y') {
-    choco install putty
+    Invoke-WebRequest 'https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.73-installer.msi' -OutFile "$HOME\Downloads\putty_installer.msi"
+    Start-Process -FilePath "$HOME\Downloads\putty_installer.msi" -PassThru -Wait
 }
 
 # Install python
 if ($InstallPython -eq 'y') {
-    choco install python
+    Invoke-WebRequest 'https://www.python.org/ftp/python/3.8.2/python-3.8.2.exe' -OutFile "$HOME\Downloads\python_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\python_installer.exe" -PassThru -Wait
     # Reload PATH
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
@@ -663,32 +677,45 @@ if ($InstallPython -eq 'y') {
 
 # Install libreoffice
 if ($InstallLibreoffice -eq 'y') {
-    choco install libreoffice-fresh
+    Invoke-WebRequest 'https://download.documentfoundation.org/libreoffice/stable/6.4.1/win/x86_64/LibreOffice_6.4.1_Win_x64.msi' -OutFile "$HOME\Downloads\libreoffice_installer.msi"
+    Start-Process -FilePath "$HOME\Downloads\libreoffice_installer.msi" -PassThru -Wait
 }
 
 # Install java
 if ($InstallJava -eq 'y') {
-    choco install jre8
+    Invoke-WebRequest 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=241536_1f5b5a70bf22433b84d0e960903adac8' -OutFile "$HOME\Downloads\java_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\java_installer.exe" -PassThru -Wait
 }
 
 # Install sysinternals
 if ($InstallSysinternals -eq 'y') {
-    choco install sysinternals
+    Invoke-WebRequest 'https://download.sysinternals.com/files/SysinternalsSuite.zip' -OutFile "$HOME\Downloads\SysinternalsSuite.zip"
+    # Extract zip folder
+    Expand-Archive -LiteralPath "$HOME\Downloads\SysinternalsSuite.zip" -DestinationPath 'C:\Program Files\SysinternalsSuite'
+    # Create shortcut
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\SysinternalsSuite.lnk')
+    $Shortcut.WorkingDirectory = 'C:\Program Files\SysinternalsSuite'
+    $Shortcut.TargetPath = 'C:\Program Files\SysinternalsSuite'
+    $Shortcut.Save()
 }
 
 # Install vscode
 if ($InstallVSCode -eq 'y') {
-    choco install vscode
+    Invoke-WebRequest 'https://aka.ms/win32-x64-user-stable' -OutFile "$HOME\Downloads\vscode_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\vscode_installer.exe" -PassThru -Wait
 }
 
 # Install wireshark
 if ($InstallWireshark -eq 'y') {
-    choco install wireshark
+    Invoke-WebRequest 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.2.2.exe' -OutFile "$HOME\Downloads\wireshark_installer.exe"
+    Start-Process -FilePath "$HOME\Downloads\wireshark_installer.exe" -PassThru -Wait
 }
 
 # Install openjdk
 if ($InstallOpenJDK -eq 'y') {
-    choco install openjdk
+    Invoke-WebRequest 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u242b08.msi' -OutFile "$HOME\Downloads\openjdk_installer.msi"
+    Start-Process -FilePath "$HOME\Downloads\openjdk_installer.msi" -PassThru -Wait
 }
 
 # Install tinynvidiaupdater
@@ -705,25 +732,25 @@ if ($InstallTinyNvidiaUpdater -eq 'y') {
 # Install firefox
 if ($InstallFirefox -eq 'y') {
     Invoke-WebRequest 'https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US' -OutFile "$HOME\Downloads\firefox.msi"
-    ."$HOME\Downloads\firefox.msi"
+    Start-Process -FilePath "$HOME\Downloads\firefox.msi" -PassThru -Wait
 }
 
 # Install chrome
 if ($InstallChrome -eq 'y') {
     Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile "$HOME\Downloads\chrome_installer.exe"
-    Start-Process -FilePath "$HOME\Downloads\chrome_installer.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\chrome_installer.exe" -PassThru -Wait
 }
 
 # Install freefilesync
 if ($InstallFreeFileSync -eq 'y') {
     Invoke-WebRequest 'https://freefilesync.org/download/FreeFileSync_10.20_Windows_Setup.exe' -OutFile "$HOME\Downloads\FreeFileSync_Windows_Setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\FreeFileSync_Windows_Setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\FreeFileSync_Windows_Setup.exe" -PassThru -Wait
 }
 
 # Install vmware player
 if ($InstallVmwarePlayer -eq 'y') {
     Invoke-WebRequest 'https://www.vmware.com/go/getplayer-win' -OutFile "$HOME\Downloads\vmware_player_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\vmware_player_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\vmware_player_setup.exe" -PassThru -Wait
 }
 
 # Install nvidiaprofileinspector
@@ -742,49 +769,49 @@ if ($InstallNvidiaProfileInspector -eq 'y') {
 # Install steam
 if ($InstallSteam -eq 'y') {
     Invoke-WebRequest 'https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe' -OutFile "$HOME\Downloads\steam_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\steam_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\steam_setup.exe" -PassThru -Wait
 }
 
 # Install origin
 if ($InstallOrigin -eq 'y') {
     Invoke-WebRequest 'https://www.dm.origin.com/download' -OutFile "$HOME\Downloads\origin_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\origin_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\origin_setup.exe" -PassThru -Wait
 }
 
 # Install gog
 if ($InstallGOG -eq 'y') {
     Invoke-WebRequest 'https://webinstallers.gog.com/download/GOG_Galaxy_2.0.exe?payload=fYZ_3idD0IMwmec-4TY3IRMzwqXwdRx0UKBBCJBP8OnhsfRaqYgGPffKJxBn8UsNiMpdHanmZrhLRtrm3U2TZ_lg95-4bfNi9tyhe-iPz1-JEjut5Pq51l3oHwGG4-oYaDaCax0I-1wi_0qxHQLI7l17-nKAoCTuiRayr5zw8lL8wgH0rKcZOf1fV7XFSo4_NFOTzVs5hpDgh2ZJlphuyCwyUyJOHIfXWEnMkIoD0qSSRW37tH_KET3LzSjdP9LekwFrtKMzSWMBTrXEeVU4-rYaYrb3ULsFxVBt2kTDCAOH9YIvMK0IZDcom4gnIghnfQkDAI-Q0QVdZK8N_Q3klZ17wA3GFaI8jh-hGmD8H9EDl6tYoygkHGUNVHMh81N5XhMIAmLE5d-3RRyvvXgZpJBRrHgj1fmG78FUntQQGpaHi-xiaZhVHJyirUsoVjpo9i3abkhSXeMZj5EXIk94r3ryDCaqRMf31-8VKvHeQrYMlhASPIKg49AB3Tt_pdPMRViiO7eSG3kZZcv5lXdRUG0LbKQ3zLtPnbYfvEQpU9_9MIe47Efopdrr8G95VHtPAi14OaQMm6Wtiu2PggA0NPHakauILGtzs9rx0C-8qDmtMzr-hVvBlcPf6XIa_3JAShscRLg0&_ga=2.163612671.1741456251.1582519704-2030968970.1574795753' -OutFile "$HOME\Downloads\gog_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\gog_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\gog_setup.exe" -PassThru -Wait
 }
 
 # Install epicstore
 if ($InstallEpicStore -eq 'y') {
     Invoke-WebRequest 'https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi' -OutFile "$HOME\Downloads\epic_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\epic_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\epic_setup.exe" -PassThru -Wait
 }
 
 # Install bethesdalauncher
 if ($InstallBethesdaLauncher -eq 'y') {
     Invoke-WebRequest 'https://download.cdp.bethesda.net/BethesdaNetLauncher_Setup.exe' -OutFile "$HOME\Downloads\bethesda_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\bethesda_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\bethesda_setup.exe" -PassThru -Wait
 }
 
 # Install BorderlessGaming
 if ($InstallBorderlessGaming -eq 'y') {
     Invoke-WebRequest 'https://github.com/Codeusa/Borderless-Gaming/releases/download/9.5.6/BorderlessGaming9.5.6_admin_setup.exe' -OutFile "$HOME\Downloads\borderless_gaming_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\borderless_gaming_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\borderless_gaming_setup.exe" -PassThru -Wait
 }
 
 # Install Discord
 if ($InstallDiscord -eq 'y') {
     Invoke-WebRequest 'https://discordapp.com/api/download?platform=win' -OutFile "$HOME\Downloads\discord_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\discord_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\discord_setup.exe" -PassThru -Wait
 }
 
 # Install FedoraMediaWriter
 if ($InstallFedoraMediaWriter -eq 'y') {
     Invoke-WebRequest 'https://getfedora.org/fmw/FedoraMediaWriter-win32-4.1.4.exe' -OutFile "$HOME\Downloads\fedora_media_writer_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\fedora_media_writer_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\fedora_media_writer_setup.exe" -PassThru -Wait
 }
 
 # Install Visual Studio Community
@@ -795,30 +822,37 @@ if ($InstallVisualStudioCommunity -eq 'y') {
 # Install OpenVPN
 if ($InstallOpenVPN -eq 'y') {
     Invoke-WebRequest 'https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.8-I602-Win10.exe' -OutFile "$HOME\Downloads\openvpn_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\openvpn_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\openvpn_setup.exe" -PassThru -Wait
 }
 
 # Install Twitch
 if ($InstallTwitch -eq 'y') {
     Invoke-WebRequest 'https://desktop.twitchsvc.net/installer/windows/TwitchSetup.exe' -OutFile "$HOME\Downloads\twitch_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\twitch_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\twitch_setup.exe" -PassThru -Wait
 }
 
 # Install Vortex
 if ($InstallVortex -eq 'y') {
-    choco install vortex
+    Invoke-WebRequest 'https://github.com/Nexus-Mods/Vortex/releases/download/v1.1.14/vortex-setup-1.1.14.exe' -OutFile "$HOME\Downloads\vortex_setup.exe"
+    Start-Process -FilePath "$HOME\Downloads\vortex_setup.exe" -PassThru -Wait
 }
 
 # Install Visual C++ Redistributables
 if ($InstallVisualRedistributables -eq 'y') {
-    choco install vcredist-all
-    choco install vcredist140
+    Invoke-WebRequest 'https://aka.ms/vs/16/release/vc_redist.x86.exe' -OutFile "$HOME\Downloads\vcredist_32_bit_2015-2019_setup.exe"
+    Start-Process -FilePath "$HOME\Downloads\vcredist_32_bit_2015-2019_setup.exe" -PassThru -Wait
+    Invoke-WebRequest 'https://aka.ms/vs/16/release/vc_redist.x64.exe' -OutFile "$HOME\Downloads\vcredist_64_bit_2015-2019_setup.exe"
+    Start-Process -FilePath "$HOME\Downloads\vcredist_64_bit_2015-2019_setup.exe" -PassThru -Wait
+    Invoke-WebRequest 'https://aka.ms/highdpimfc2013x86enu' -OutFile "$HOME\Downloads\vcredist_32_bit_2013_setup.exe"
+    Start-Process -FilePath "$HOME\Downloads\vcredist_32_bit_2013_setup.exe" -PassThru -Wait
+    Invoke-WebRequest 'https://aka.ms/highdpimfc2013x64enu' -OutFile "$HOME\Downloads\vcredist_64_bit_2013_setup.exe"
+    Start-Process -FilePath "$HOME\Downloads\vcredist_64_bit_2013_setup.exe" -PassThru -Wait
 }
 
 # Install RockstarLauncher
 if ($InstallRockstarLauncher -eq 'y') {
     Invoke-WebRequest 'https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe#_ga=2.130661071.1975042814.1582524189-583909228.1582524189' -OutFile "$HOME\Downloads\rockstar_setup.exe"
-    Start-Process -FilePath "$HOME\Downloads\rockstar_setup.exe" -PassThru
+    Start-Process -FilePath "$HOME\Downloads\rockstar_setup.exe" -PassThru -Wait
 }
 
 # To Update all installed choclatey packages use command:
