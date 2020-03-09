be6f41832b327ff03180c0b7157be8b720a5d830
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-03-09 12:34:58 -0400

    Added PATH.  Script will delete backups older than 180 days.

--- linux_scripts/backup_configs.sh
+++ linux_scripts/backup_configs.sh
@@ -4,6 +4,9 @@
 # Add * 0 * * 1 bash /usr/local/bin/backup_configs.sh & to cron
 
 function backup_configs() {
+    # Define path to commands.
+    PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
+
     # Get username
     user_name=$(logname)
 
@@ -13,6 +16,10 @@ function backup_configs() {
     # Create tar archive compressed with gzip
     tar -czf "/home/$user_name/config_backup_$time.tar.gz" '/etc'
     tar -czf "/home/$user_name/home_backup_$time.tar.gz" '/home'
+
+    # Delete backups older than 180 days
+    find "/home/$user_name" -name 'config_backup_*.tar.gz' -mtime +180 -delete
+    find "/home/$user_name" -name 'home_backup_*.tar.gz' -mtime +180 -delete
 }
 
 # Call functions
