9a410110392f21bf1f03fba18de417d51116dd2d
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-03-12 16:48:45 -0400

    Changed P to p.

--- windows_scripts/configure_windows_10.ps1
+++ windows_scripts/configure_windows_10.ps1
@@ -900,7 +900,7 @@ function ConfigurePowerOptions {
     powercfg /change -monitor-timeout-ac 5
     powercfg /change -monitor-timeout-dc 5
     # Set standby timeout
-    Powercfg /change -standby-timeout-ac 0
+    powercfg /change -standby-timeout-ac 0
     powercfg /change -standby-timeout-dc 0
 }
 
