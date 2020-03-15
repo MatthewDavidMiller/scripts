4b3bd6b33fd28bca8fc3b034e918df5991feabc6
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-03-14 20:19:33 -0400

    Enabled solid compression.

--- windows_scripts/7z_batch_compress.ps1
+++ windows_scripts/7z_batch_compress.ps1
@@ -8,7 +8,7 @@ $Source = Read-Host 'Specify source directory to batch compress '
 # Compress folders
 if ($CompressFolders -eq 'y') {
     foreach ($item in (Get-ChildItem -Directory -Name $Source)) {
-        & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=off" a "$Source\$item.7z" "$Source\$item"
+        & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$Source\$item.7z" "$Source\$item"
 
     }
 }
@@ -16,7 +16,7 @@ if ($CompressFolders -eq 'y') {
 # Compress files
 if ($CompressFiles -eq 'y') {
     foreach ($item in (Get-ChildItem -File -Name $Source)) {
-        & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=off" a "$Source\$item.7z" "$Source\$item"
+        & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$Source\$item.7z" "$Source\$item"
 
     }
 }
