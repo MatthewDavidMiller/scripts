5f37ab21ce4b49e7fced27d47fa6c82df10c9f01
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-03-17 16:23:41 -0400

    Added storage configuration.  Added work in progress vm configuration.

--- linux_scripts/proxmox_configure.sh
+++ linux_scripts/proxmox_configure.sh
@@ -45,9 +45,20 @@ function configure_scripts() {
     mv 'backup_configs.sh' '/usr/local/bin/backup_configs.sh'
     chmod +x '/usr/local/bin/backup_configs.sh'
 
+    # Script to backup vms
+    cat <<EOF >'/usr/local/bin/vm_backup.sh'
+#!/bin/bash
+PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+
+vzdump 100 101 102 --mode snapshot --quiet 1 --mailnotification always --compress lzo --storage vm_backups
+
+EOF
+    chmod +x '/usr/local/bin/vm_backup.sh'
+
     # Configure cron jobs
     cat <<EOF >jobs.cron
 * 0 * * 1 bash /usr/local/bin/backup_configs.sh &
+0 0 * * 6 bash /usr/local/bin/vm_backup.sh &
 
 EOF
     crontab jobs.cron
@@ -134,27 +145,28 @@ function configure_proxmox() {
     apt-get remove os-prober
     apt-get remove linux-image-amd64
     update-grub
+}
 
-    function configure_proxmox_network() {
-        #Prompts
-        # Set server ip
-        read -r -p "Enter server ip address. Example '10.1.10.3': " ip_address
-        # Set network
-        read -r -p "Enter network ip address. Example '10.1.10.0': " network_address
-        # Set subnet mask
-        read -r -p "Enter netmask. Example '255.255.255.0': " subnet_mask
-        # Set gateway
-        read -r -p "Enter gateway ip. Example '10.1.10.1': " gateway_address
-        # Set dns server
-        read -r -p "Enter dns server ip. Example '10.1.10.5': " dns_address
+function configure_proxmox_network() {
+    #Prompts
+    # Set server ip
+    read -r -p "Enter server ip address. Example '10.1.10.3': " ip_address
+    # Set network
+    read -r -p "Enter network ip address. Example '10.1.10.0': " network_address
+    # Set subnet mask
+    read -r -p "Enter netmask. Example '255.255.255.0': " subnet_mask
+    # Set gateway
+    read -r -p "Enter gateway ip. Example '10.1.10.1': " gateway_address
+    # Set dns server
+    read -r -p "Enter dns server ip. Example '10.1.10.5': " dns_address
 
-        # Get the interface name
-        interface="$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')"
-        echo "Interface name is ${interface}"
+    # Get the interface name
+    interface="$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')"
+    echo "Interface name is ${interface}"
 
-        # Configure network
-        rm -f '/etc/network/interfaces'
-        cat <<EOF >'/etc/network/interfaces'
+    # Configure network
+    rm -f '/etc/network/interfaces'
+    cat <<EOF >'/etc/network/interfaces'
 auto lo
 iface lo inet loopback
 
@@ -172,12 +184,59 @@ iface vmbr0 inet static
         bridge-fd 0
 EOF
 
-        # Restart network interface
-        ifdown "${interface}" && ifup "${interface}"
-    }
+    # Restart network interface
+    ifdown "${interface}" && ifup "${interface}"
+}
 
-    # Call functions
-    configure_proxmox_network
+function configure_proxmox_storage() {
+    rm -f '/etc/pve/storage.cfg'
+    cat <<EOF >'/etc/pve/storage.cfg'
+dir: local
+        path /var/lib/vz
+        content vztmpl,iso
+        shared 0
+
+lvmthin: local-lvm
+        thinpool data
+        vgname pve
+        content images,rootdir
+
+cifs: vm_backups
+        path /mnt/pve/vm_backups
+        server 10.1.10.4
+        share vm_backup
+        content backup
+        domain matt-nas.miller.lan
+        maxfiles 3
+        username matthew
+EOF
+}
+
+# Work in progress function
+function configure_proxmox_vms() {
+    # ISO location
+    debian_iso='local:iso/debian_standard.iso'
+    # ISO checksum
+    debian_checksum='3019ed00dfffa992066dcf4eada6b9270687345e709d4c6f07fee63f85e8bafc'
+
+    # Get debian iso
+    wget 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-10.3.0-amd64-standard.iso' -O '/var/lib/vz/template/iso/debian_standard.iso'
+    # Verify Image
+    if sha256sum '/var/lib/vz/template/iso/debian_standard.iso' | awk "${1}"=="${debian_checksum}"; then
+        print 'Image Verified'
+    else
+        read -r -p "Image is not valid, exiting script: "
+        exit
+    fi
+
+    # Configure nas vm
+    qm create 100 -scsi0 local-lvm:13 -net0 virtio -balloon 512 -bios ovmf -cores 1 -cpu host -bootdisk scsi0 -efidisk0 local-lvm: -name matt-nas -memory 4096 -numa 0 -onboot 1 -ostype l26 -sockets 1 -cdrom ${debian_iso}
+
+    # Configure pihole vm
+    qm create 101 -scsi0 local-lvm:13 -net0 virtio -balloon 512 -bios ovmf -cores 1 -cpu host -bootdisk scsi0 -efidisk0 local-lvm: -name matt-pihole -memory 4096 -numa 0 -onboot 1 -ostype l26 -sockets 1 -cdrom ${debian_iso}
+
+    #Configure vpn vm
+    qm create 103 -scsi0 local-lvm:13 -net0 virtio -balloon 512 -bios ovmf -cores 1 -cpu host -bootdisk scsi0 -efidisk0 local-lvm: -name matt-vpn -memory 2048 -numa 0 -onboot 1 -ostype l26 -sockets 1 -cdrom ${debian_iso}
 }
 
 # Call functions
@@ -188,3 +247,5 @@ configure_scripts
 configure_auto_updates
 configure_hosts
 configure_proxmox
+configure_proxmox_network
+configure_proxmox_storage
