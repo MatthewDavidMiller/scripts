fefb4072f23f54ef3ab3076e4aa5c5a93d05f23b
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-03-29 16:23:09 -0400

    Rewrote script to set tasks in an Idle state.

--- windows_scripts/7z_batch_compress.ps1
+++ windows_scripts/7z_batch_compress.ps1
@@ -1,27 +1,38 @@
 # Script to batch compress files with 7zip.
 
 # Prompts
-$CompressFiles = Read-Host 'Compress files? y/n '
-$CompressFolders = Read-Host 'Compress folders? y/n '
+$CompressFilesVar = Read-Host 'Compress files? y/n '
+$CompressFoldersVar = Read-Host 'Compress folders? y/n '
 $Source = Read-Host 'Specify source directory to batch compress '
 $Destination = Read-Host 'Specify destination directory '
 
-# Compress folders
-if ($CompressFolders -eq 'y') {
+function CompressFolders {
     foreach ($item in (Get-ChildItem -Directory -Name $Source)) {
-        & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$Destination\$item.7z" "$Source\$item"
+        $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:Destination\$using:item.7z" "$using:Source\$using:item" }
         $7zProcess = Get-Process -Name "7z"
         $7zProcess.PriorityClass = 'Idle'
+        Get-Job | Wait-Job
+        Receive-Job -Job $7zJob
     }
 }
 
-# Compress files
-if ($CompressFiles -eq 'y') {
+function CompressFiles {
     foreach ($item in (Get-ChildItem -File -Name $Source)) {
-        & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$Destination\$item.7z" "$Source\$item"
+        $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:Destination\$using:item.7z" "$using:Source\$using:item" }
         $7zProcess = Get-Process -Name "7z"
         $7zProcess.PriorityClass = 'Idle'
+        Get-Job | Wait-Job
+        Receive-Job -Job $7zJob
     }
+}
+
+# Call functions
+if ($CompressFoldersVar -eq 'y') {
+    CompressFolders
+}
+
+if ($CompressFilesVar -eq 'y') {
+    CompressFiles
 }
 
 Read-Host -Prompt "Script finished.  Press enter to exit."
