984a0900fca9e4bde3b33e9ff4c6e3ed5a87122f
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-03-29 17:54:24 -0400

    Added option to do more batch compressions.

--- windows_scripts/7z_batch_compress.ps1
+++ windows_scripts/7z_batch_compress.ps1
@@ -3,10 +3,10 @@
 # Prompts
 $CompressFilesVar = Read-Host 'Compress files? y/n '
 $CompressFoldersVar = Read-Host 'Compress folders? y/n '
-$Source = Read-Host 'Specify source directory to batch compress '
-$Destination = Read-Host 'Specify destination directory '
 
 function CompressFolders {
+    $Source = Read-Host 'Specify source directory to batch compress '
+    $Destination = Read-Host 'Specify destination directory '
     foreach ($item in (Get-ChildItem -Directory -Name $Source)) {
         $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:Destination\$using:item.7z" "$using:Source\$using:item" }
         $7zProcess = Get-Process -Name "7z"
@@ -14,9 +14,24 @@ function CompressFolders {
         Get-Job | Wait-Job
         Receive-Job -Job $7zJob
     }
+    $AnotherTask = Read-Host 'Compress more folders? y/n '
+    DO {
+        $Source = Read-Host 'Specify source directory to batch compress '
+        $Destination = Read-Host 'Specify destination directory '
+        foreach ($item in (Get-ChildItem -Directory -Name $Source)) {
+            $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:Destination\$using:item.7z" "$using:Source\$using:item" }
+            $7zProcess = Get-Process -Name "7z"
+            $7zProcess.PriorityClass = 'Idle'
+            Get-Job | Wait-Job
+            Receive-Job -Job $7zJob
+        }
+        $AnotherTask = Read-Host 'Compress more folders? y/n '
+    } while ($AnotherTask -eq 'y')
 }
 
 function CompressFiles {
+    $Source = Read-Host 'Specify source directory to batch compress '
+    $Destination = Read-Host 'Specify destination directory '
     foreach ($item in (Get-ChildItem -File -Name $Source)) {
         $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:Destination\$using:item.7z" "$using:Source\$using:item" }
         $7zProcess = Get-Process -Name "7z"
@@ -24,6 +39,19 @@ function CompressFiles {
         Get-Job | Wait-Job
         Receive-Job -Job $7zJob
     }
+    $AnotherTask = Read-Host 'Compress more files? y/n '
+    DO {
+        $Source = Read-Host 'Specify source directory to batch compress '
+        $Destination = Read-Host 'Specify destination directory '
+        foreach ($item in (Get-ChildItem -File -Name $Source)) {
+            $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:Destination\$using:item.7z" "$using:Source\$using:item" }
+            $7zProcess = Get-Process -Name "7z"
+            $7zProcess.PriorityClass = 'Idle'
+            Get-Job | Wait-Job
+            Receive-Job -Job $7zJob
+        }
+        $AnotherTask = Read-Host 'Compress more files? y/n '
+    } while ($AnotherTask -eq 'y')
 }
 
 # Call functions
