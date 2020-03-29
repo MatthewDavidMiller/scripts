ece9cea618af8230cea3e202e0b2732763d5df7b
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-03-29 03:53:37 -0400

    Changed priority to idle.

--- windows_scripts/7z_batch_compress.ps1
+++ windows_scripts/7z_batch_compress.ps1
@@ -9,7 +9,8 @@ $Source = Read-Host 'Specify source directory to batch compress '
 if ($CompressFolders -eq 'y') {
     foreach ($item in (Get-ChildItem -Directory -Name $Source)) {
         & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$Source\$item.7z" "$Source\$item"
-
+        $7zProcess = Get-Process -Name "7z"
+        $7zProcess.PriorityClass = 'Idle'
     }
 }
 
@@ -17,7 +18,8 @@ if ($CompressFolders -eq 'y') {
 if ($CompressFiles -eq 'y') {
     foreach ($item in (Get-ChildItem -File -Name $Source)) {
         & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$Source\$item.7z" "$Source\$item"
-
+        $7zProcess = Get-Process -Name "7z"
+        $7zProcess.PriorityClass = 'Idle'
     }
 }
 
