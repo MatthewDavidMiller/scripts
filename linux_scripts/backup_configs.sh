04601e70d01726678fb01237a9712ef736f65be8
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-04-27 01:52:31 -0400

    Fixed the issue of backing up the backups, which could lead to very large file sizes. Changed file deletion age to 30 days from 180 days.

--- linux_scripts/backup_configs.sh
+++ linux_scripts/backup_configs.sh
@@ -26,12 +26,12 @@ function backup_configs() {
     time=$(date +"%m_%d_%Y")
 
     # Create tar archive compressed with gzip
-    tar -czf "/home/$user_name/config_backup_$time.tar.gz" '/etc'
-    tar -czf "/home/$user_name/home_backup_$time.tar.gz" '/home'
+    tar --exclude 'config_backup_*' --exclude 'home_backup*' -czf "/home/$user_name/config_backup_$time.tar.gz" '/etc'
+    tar --exclude 'config_backup_*' --exclude 'home_backup*' -czf "/home/$user_name/home_backup_$time.tar.gz" '/home'
 
-    # Delete backups older than 180 days
-    find "/home/$user_name" -name 'config_backup_*.tar.gz' -mtime +180 -delete
-    find "/home/$user_name" -name 'home_backup_*.tar.gz' -mtime +180 -delete
+    # Delete backups older than 30 days
+    find "/home/$user_name" -name 'config_backup_*.tar.gz' -mtime +30 -delete
+    find "/home/$user_name" -name 'home_backup_*.tar.gz' -mtime +30 -delete
 }
 
 # Call functions
