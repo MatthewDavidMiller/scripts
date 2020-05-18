4787e9527b7d304da2bae72fcc8ca519eef893a4
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-05-17 20:44:29 -0400

    Added wireguard configuration.

--- linux_scripts/vpn_server_configure.sh
+++ linux_scripts/vpn_server_configure.sh
@@ -19,6 +19,9 @@ source linux_install_scripts.sh
 source ssh_scripts.sh
 source ufw_scripts.sh
 
+# Prompts
+read -r -p "Generate more than one wireguard client configs? [y/N] " wireguard_clients_response
+
 # Default variables
 release_name='buster'
 key_name='vpn_key'
@@ -39,6 +42,15 @@ allow_netbios='n'
 limit_port_64640='y'
 allow_port_8006='n'
 allow_omada_controller='n'
+wireguard_interface='wg0'
+wireguard_server_ip_address='10.3.0.1'
+wireguard_server_vpn_key_name='wireguard_vpn_server'
+wireguard_client_key_name='matthew_wireguard'
+wireguard_server_listen_port='64640'
+wireguard_client_ip_address='10.3.0.2'
+wireguard_client_password=''
+wireguard_dns_server='10.1.10.5'
+wireguard_public_dns_ip_address='mattm.mooo.com'
 
 # Call functions
 lock_root
@@ -51,7 +63,30 @@ configure_ssh
 generate_ssh_key "${user_name}" "y" "n" "n" "${key_name}"
 configure_ufw_base
 ufw_configure_rules "${network_prefix}" "${limit_ssh}" "${allow_dns}" "${allow_unbound}" "${allow_http}" "${allow_https}" "${allow_port_4711_tcp}" "${allow_smb}" "${allow_netbios}" "${limit_port_64640}" "${allow_port_8006}" "${allow_omada_controller}"
+ufw_allow_default_forward
+ufw_allow_ip_forwarding
 configure_vpn_scripts
-configure_vpn
+setup_basic_wireguard_interface "${wireguard_interface}" "${wireguard_server_ip_address}"
+generate_wireguard_key "${user_name}" "${wireguard_server_vpn_key_name}"
+configure_wireguard_server_base "${wireguard_interface}" "${user_name}" "${wireguard_server_vpn_key_name}" "${wireguard_server_ip_address}" "${wireguard_server_listen_port}"
+generate_wireguard_key "${user_name}" "${wireguard_client_key_name}"
+add_wireguard_peer "${wireguard_interface}" "${user_name}" "${wireguard_server_vpn_key_name}" "${wireguard_client_ip_address}" "${wireguard_client_password}"
+wireguard_create_client_config "${wireguard_interface}" "${user_name}" "${wireguard_client_key_name}" "${wireguard_server_vpn_key_name}" "${wireguard_client_ip_address}" "${wireguard_client_password}" "${wireguard_dns_server}" "${wireguard_public_dns_ip_address}" "${wireguard_server_listen_port}"
+
+while [[ "${wireguard_clients_response}" =~ ^([yY][eE][sS]|[yY])+$ ]]; do
+    read -r -p "Enter wireguard client name: [y/N] " wireguard_client_key_name
+    read -r -p "Enter wireguard client ip address: [y/N] " wireguard_client_ip_address
+    read -r -p "Enter wireguard client password: [y/N] " wireguard_client_password
+    generate_wireguard_key "${user_name}" "${wireguard_client_key_name}"
+    add_wireguard_peer "${wireguard_interface}" "${user_name}" "${wireguard_server_vpn_key_name}" "${wireguard_client_ip_address}" "${wireguard_client_password}"
+    wireguard_create_client_config "${wireguard_interface}" "${user_name}" "${wireguard_client_key_name}" "${wireguard_server_vpn_key_name}" "${wireguard_client_ip_address}" "${wireguard_client_password}" "${wireguard_dns_server}" "${wireguard_public_dns_ip_address}" "${wireguard_server_listen_port}"
+    read -r -p "Add another wireguard client? [y/N] " wireguard_clients_loop_response
+
+    if [[ "${wireguard_clients_loop_response}" =~ ^([nN][oO]|[nN])+$ ]]; then
+        break
+    fi
+done
+
 apt_configure_auto_updates "${release_name}"
 enable_ufw
+enable_wireguard_service "${wireguard_interface}"
