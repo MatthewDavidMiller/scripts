9acf62db8979b96fadc0a207ced1c42d23f9deb5
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-07-17 14:54:16 -0400

    Added some basic configuration.

--- linux_scripts/log_rotate_setup.sh
+++ linux_scripts/log_rotate_setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # Setup log rotate to keep log files from getting too large in size.
+
+function get_username() {
+    user_name=$(logname)
+}
+
+function log_rotate_config_setup() {
+    touch -a '/etc/logrotate.conf'
+    grep -q "^\s*[#]*\s*daily|weekly|monthly\s*$" '/etc/logrotate.conf' && sed -i "s,^\s*[#]*\s*daily|weekly|monthly\s*$,daily," '/etc/logrotate.conf' || printf '%s\n' 'daily' >>'/etc/logrotate.conf'
+    grep -q "^\s*[#]*\s*minsize.*$" '/etc/logrotate.conf' && sed -i "s,^\s*[#]*\s*minsize.*$,minsize 100M," '/etc/logrotate.conf' || printf '%s\n' 'minsize 100M' >>'/etc/logrotate.conf'
+    grep -q "^\s*[#]*\s*rotate\s*[0-9]*$" '/etc/logrotate.conf' && sed -i "s,^\s*[#]*\s*rotate\s*[0-9]*$,rotate 4," '/etc/logrotate.conf' || printf '%s\n' 'rotate 4' >>'/etc/logrotate.conf'
+    grep -q "^\s*[#]*\s*compress\s*$" '/etc/logrotate.conf' && sed -i "s,^\s*[#]*\s*compress\s*$,compress," '/etc/logrotate.conf' || printf '%s\n' 'compress' >>'/etc/logrotate.conf'
+
+}
