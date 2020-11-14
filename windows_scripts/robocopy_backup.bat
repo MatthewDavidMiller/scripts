7ff68e408794fd53ed67342d49f92994f0ca1165
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-11-13 21:05:07 -0500

    Updated robocopy script.

--- windows_scripts/robocopy_backup.bat
+++ windows_scripts/robocopy_backup.bat
@@ -1,9 +1,17 @@
-rem Used to backup files
-rem Run the script as administrator
+rem Robocopy backup script
 
 rem Set source, destination, and log file location
-SET source_directory="source_location"
-SET destination_directory="destination_location"
+SET MainBackupSource=""
+SET MainBackupDestination=""
 SET log_file="log_file"
 
-robocopy %source_directory% %destination_directory% /s /copyall /np /r:2 /w:5 /tee /log:%log_file%
+rem Set two more backup source and destinations
+SET Backup2Source=""
+SET Backup2Destination=""
+SET Backup3Source=""
+SET Backup3Destination=""
+
+rem Run the robocopy commands
+robocopy %Backup3Source% %Backup3Destination% /s /copyall /np /r:2 /w:5 /tee /log:%log_file%
+robocopy %Backup2Source% %Backup2Destination% /s /copyall /np /r:2 /w:5 /tee /log:%log_file%
+robocopy %MainBackupSource% %MainBackupDestination% /s /copyall /np /r:2 /w:5 /tee /mir /log:%log_file%
