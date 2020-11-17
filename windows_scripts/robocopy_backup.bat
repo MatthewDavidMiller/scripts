9f041f202ea7e218c022a01b2333d5890c047386
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-11-16 19:22:34 -0500

    Removed second and third backups.

--- windows_scripts/robocopy_backup.bat
+++ windows_scripts/robocopy_backup.bat
@@ -5,13 +5,5 @@ SET MainBackupSource=""
 SET MainBackupDestination=""
 SET log_file="log_file"
 
-rem Set two more backup source and destinations
-SET Backup2Source=""
-SET Backup2Destination=""
-SET Backup3Source=""
-SET Backup3Destination=""
-
 rem Run the robocopy commands
-robocopy %Backup3Source% %Backup3Destination% /s /copy:DAT /np /r:2 /w:5 /tee /MT:40 /log:%log_file%
-robocopy %Backup2Source% %Backup2Destination% /s /copy:DAT /np /r:2 /w:5 /tee /MT:40 /log:%log_file%
 robocopy %MainBackupSource% %MainBackupDestination% /s /copy:DAT /np /r:2 /w:5 /tee /MT:40 /mir /log:%log_file%
