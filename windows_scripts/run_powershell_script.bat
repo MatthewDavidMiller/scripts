edc19119cfb84d952b363be889dc0ca9663b3f0a
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-12-18 22:42:20 -0500

    Updated script to open a file browser.

--- windows_scripts/run_powershell_script.bat
+++ windows_scripts/run_powershell_script.bat
@@ -1,9 +1,25 @@
 :: Used to run a powershell script without needing to change the execution policy
 :: Credits to Zack Olinske, https://stackoverflow.com/questions/42897554/powershell-executionpolicy-change-bypass
 :: Credits to Microsoft, https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/set_1
+:: Credits to rojo, https://stackoverflow.com/questions/15885132/file-folder-chooser-dialog-from-a-windows-batch-script
 
-:: Specify file
-SET /p File=Enter full script path:
+:: powershell.exe -executionpolicy bypass -file %%~I
 
-:: Run powershell script
-powershell.exe -executionpolicy bypass -file "%File%"
+<# : "RunPowerShell.bat"
+@echo off
+setlocal
+
+for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+    powershell.exe -executionpolicy bypass -file %%~I
+)
+goto :EOF
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+$filebrowser = new-object Windows.Forms.OpenFileDialog
+$filebrowser.InitialDirectory = pwd
+$filebrowser.Filter = "Powershell Scripts (*.ps1)|*.ps1|All Files (*.*)|*.*"
+$filebrowser.ShowHelp = $true
+$filebrowser.Multiselect = $false
+[void]$filebrowser.ShowDialog()
+if ($filebrowser.Multiselect) { $filebrowser.FileNames } else { $filebrowser.FileName }
