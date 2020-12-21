58957d0a8a1c9059cce3c42939db1e3e9a0c3f70
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-12-20 22:41:15 -0500

    Refactored get file hashes script.

--- windows_scripts/get_file_hashes.ps1
+++ windows_scripts/get_file_hashes.ps1
@@ -1,35 +1,104 @@
 # Script used to get the hash of a file.
 
-function GetFileHashes {
-    # Prompts
-    $MD5 = Read-Host 'Get MD5 checksums? y/n '
-    $SHA1 = Read-Host 'Get SHA1 checksums? y/n '
-    $SHA256 = Read-Host 'Get SHA256 checksums? y/n '
-    $SHA512 = Read-Host 'Get SHA512 checksums? y/n '
-    $Source = Read-Host 'Specify source directory to get file hashes '
+function GetMD5Hash {
+    Add-Type -AssemblyName System.Windows.Forms
+    $filebrowser = New-Object Windows.Forms.OpenFileDialog
+    $filebrowser.InitialDirectory = Get-Location
+    $filebrowser.Filter = "All Files (*.*)|*.*"
+    $filebrowser.ShowHelp = $true
+    $filebrowser.Multiselect = $true
+    [void]$filebrowser.ShowDialog()
+    if ($filebrowser.Multiselect) { $Source = @($filebrowser.FileNames) } else { $Source = @($filebrowser.FileName) }
 
     # Get MD5 checksums
-    if ($MD5 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm MD5 | Format-List
+    foreach ($item in ($Source)) {
+        Get-FileHash "$item" -Algorithm MD5 | Format-List
     }
+}
 
-    # Get SHA1 checksums
-    if ($SHA1 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm SHA1 | Format-List
+function GetSHA1Hash {
+    Add-Type -AssemblyName System.Windows.Forms
+    $filebrowser = New-Object Windows.Forms.OpenFileDialog
+    $filebrowser.InitialDirectory = Get-Location
+    $filebrowser.Filter = "All Files (*.*)|*.*"
+    $filebrowser.ShowHelp = $true
+    $filebrowser.Multiselect = $true
+    [void]$filebrowser.ShowDialog()
+    if ($filebrowser.Multiselect) { $Source = @($filebrowser.FileNames) } else { $Source = @($filebrowser.FileName) }
+
+    # Get MD5 checksums
+    foreach ($item in ($Source)) {
+        Get-FileHash "$item" -Algorithm SHA1 | Format-List
     }
+}
 
-    # Get SHA256 checksums
-    if ($SHA256 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm SHA256 | Format-List
+function GetSHA256Hash {
+    Add-Type -AssemblyName System.Windows.Forms
+    $filebrowser = New-Object Windows.Forms.OpenFileDialog
+    $filebrowser.InitialDirectory = Get-Location
+    $filebrowser.Filter = "All Files (*.*)|*.*"
+    $filebrowser.ShowHelp = $true
+    $filebrowser.Multiselect = $true
+    [void]$filebrowser.ShowDialog()
+    if ($filebrowser.Multiselect) { $Source = @($filebrowser.FileNames) } else { $Source = @($filebrowser.FileName) }
+
+    # Get MD5 checksums
+    foreach ($item in ($Source)) {
+        Get-FileHash "$item" -Algorithm SHA256 | Format-List
     }
+}
 
-    # Get SHA512 checksums
-    if ($SHA512 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm SHA512 | Format-List
+function GetSHA512Hash {
+    Add-Type -AssemblyName System.Windows.Forms
+    $filebrowser = New-Object Windows.Forms.OpenFileDialog
+    $filebrowser.InitialDirectory = Get-Location
+    $filebrowser.Filter = "All Files (*.*)|*.*"
+    $filebrowser.ShowHelp = $true
+    $filebrowser.Multiselect = $true
+    [void]$filebrowser.ShowDialog()
+    if ($filebrowser.Multiselect) { $Source = @($filebrowser.FileNames) } else { $Source = @($filebrowser.FileName) }
+
+    # Get MD5 checksums
+    foreach ($item in ($Source)) {
+        Get-FileHash "$item" -Algorithm SHA512 | Format-List
     }
+}
 
-    Read-Host -Prompt "Files Hashed.  Press enter to continue."
+function InteractiveMenu {
+    function Show-Menu {
+        param (
+            [string]$Title = 'Configuration Options'
+        )
+        Clear-Host
+        Write-Host "================ $Title ================"
+
+        Write-Host "1: Press '1' to get Md5 hash."
+        Write-Host "2: Press '2' to get SHA1 hash."
+        Write-Host "3: Press '3' to get SHA256 hash."
+        Write-Host "4: Press '4' to get SHA512 hash."
+        Write-Host "q: Press 'q' to quit."
+    }
+    do {
+        Show-Menu
+        $selection = Read-Host "Select an option"
+        switch ($selection) {
+            '1' {
+                GetMD5Hash
+            }
+            '2' {
+                GetSHA1Hash
+            }
+            '3' {
+                GetSHA256Hash
+            }
+            '4' {
+                GetSHA512Hash
+            }
+        }
+        Pause
+    }
+    until ($selection -eq 'q')
 }
 
 # Call functions
-GetFileHashes
+InteractiveMenu
