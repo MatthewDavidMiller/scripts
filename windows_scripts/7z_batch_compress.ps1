f18de70832184fe895aa91389851a02720823426
MatthewDavidMiller <MatthewMiller@mdmiller.dev>
2021-05-09 19:16:20 -0400

    Suppressed errors. Added compressing all folders in a directory. Added if statement.

--- windows_scripts/7z_batch_compress.ps1
+++ windows_scripts/7z_batch_compress.ps1
@@ -1,6 +1,9 @@
 # Script to batch compress files with 7zip.
-# Credits to Spc_555, https://stackoverflow.com/questions/25690038/how-do-i-properly-use-the-folderbrowserdialog-in-powershell
-# Credits to Microsoft, https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.folderbrowserdialog?view=net-5.0
+# Credits
+# Spc_555, https://stackoverflow.com/questions/25690038/how-do-i-properly-use-the-folderbrowserdialog-in-powershell
+# https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.folderbrowserdialog?view=net-5.0
+# https://stackoverflow.com/questions/11969596/how-do-i-suppress-standard-error-output-in-powershell
+# https://github.com/PowerShell/PowerShell/issues/10875
 
 function CompressFolders {
 
@@ -18,10 +21,38 @@ function CompressFolders {
     foreach ($item in ($Source)) {
         $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:item.7z" "$using:item" }
         Start-Sleep -s 12
-        $7zProcess = Get-Process -Name "7z"
-        $7zProcess.PriorityClass = 'Idle'
-        Get-Job | Wait-Job
-        Receive-Job -Job $7zJob
+        $7zProcess = Get-Process -Name "7z" -ErrorAction SilentlyContinue
+        if ( $7zProcess.PriorityClass ) {
+            $7zProcess.PriorityClass = 'Idle'
+        }
+        Get-Job | Wait-Job -ErrorAction SilentlyContinue
+        Receive-Job -Job $7zJob -ErrorAction SilentlyContinue
+    }
+}
+
+function CompressAllFolders {
+
+    function GetFolderBrowser {
+        Add-Type -AssemblyName System.Windows.Forms
+        $FolderBrowser = New-Object Windows.Forms.FolderBrowserDialog
+        $FolderBrowser.RootFolder = "MyComputer"
+        $FolderBrowser.Description = "Select directory to compress all folders in"
+        [void]$FolderBrowser.ShowDialog()
+        $SelectedFolder = $FolderBrowser.SelectedPath
+        return $SelectedFolder
+    }
+    $Source = GetFolderBrowser
+
+    foreach ( $item in ( Get-Childitem -Path $Source ) ) {
+        $7zfolder = "$Source\$item"
+        $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:7zfolder.7z" "$using:7zfolder" }
+        Start-Sleep -s 12
+        $7zProcess = Get-Process -Name "7z" -ErrorAction SilentlyContinue
+        if ( $7zProcess.PriorityClass ) {
+            $7zProcess.PriorityClass = 'Idle'
+        }
+        Get-Job | Wait-Job -ErrorAction SilentlyContinue
+        Receive-Job -Job $7zJob -ErrorAction SilentlyContinue
     }
 }
 
@@ -42,10 +73,12 @@ function CompressFiles {
     foreach ($item in ($Source)) {
         $7zJob = Start-Job -ScriptBlock { & "c:\Program Files\7-Zip\7z.exe" "-mx=9" "-ms=on" a "$using:item.7z" "$using:item" }
         Start-Sleep -s 12
-        $7zProcess = Get-Process -Name "7z"
-        $7zProcess.PriorityClass = 'Idle'
-        Get-Job | Wait-Job
-        Receive-Job -Job $7zJob
+        $7zProcess = Get-Process -Name "7z" -ErrorAction SilentlyContinue
+        if ( $7zProcess.PriorityClass ) {
+            $7zProcess.PriorityClass = 'Idle'
+        }
+        Get-Job | Wait-Job -ErrorAction SilentlyContinue
+        Receive-Job -Job $7zJob -ErrorAction SilentlyContinue
     }
 }
 
@@ -59,6 +92,7 @@ function InteractiveMenu {
 
         Write-Host "1: Press '1' to compress files."
         Write-Host "2: Press '2' to compress folders."
+        Write-Host "3: Press '3' to compress all folders in a directory."
         Write-Host "q: Press 'q' to quit."
     }
     do {
@@ -70,6 +104,9 @@ function InteractiveMenu {
             }
             '2' {
                 CompressFolders
+            }
+            '3' {
+                CompressAllFolders
             }
         }
         Pause
