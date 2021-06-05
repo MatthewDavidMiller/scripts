c6f9741f43f11ffcce1376edae745c5ac46e79d3
MatthewDavidMiller <MatthewMiller@mdmiller.dev>
2021-06-05 17:15:46 -0400

    Changed default destination.

--- windows_scripts/backup_env_files.ps1
+++ windows_scripts/backup_env_files.ps1
@@ -1,5 +1,5 @@
 $Source = 'C:\Users\matthew\Nextcloud\git'
-$Destination = 'C:\Users\matthew\iCloudDrive\Matthew_Cloud\git_env'
+$Destination = 'C:\Users\matthew\Google Drive\Matthew_Cloud\git_env'
 
 foreach ( $item in ( Get-ChildItem -Name $Source ) ) {
     & robocopy "$Source\$item\env" "$Destination\$item\env" /s /copy:DAT /np /r:2 /w:5 /MT:40 /tee /mir
